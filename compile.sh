36bf649f95e5fa73ded165d8af8e39d440b8556f
Beha <shacknetisp@mail.com>
2018-11-02 08:42:09 -0400

    Initial commit.

---
+++ compile.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+test -e compile.sh || exit 1
+
+TMP="/tmp/crc_$RANDOM"
+touch "$TMP"
+
+echo "#-#-# Automatically Compiled Units"
+for i in url/*; do
+    test -e "$i" || continue;
+    echo
+    echo "#-#-# Unit:" $(basename $i)
+    curl $(cat $i) > $TMP && {
+        cat $TMP > url_cache/$(basename $i)
+    }
+    cat url_cache/$(basename $i)
+done
+
+for i in insert/*; do
+    test -e "$i" || continue;
+
+    echo
+    echo "#-#-# Unit:" $(basename $i)
+    cat $i
+done
+
+for i in script/*; do
+    test -e "$i" || continue;
+
+    echo
+    echo "#-#-# Unit:" $(basename $i)
+    bash $i
+done
+
+rm "$TMP"
