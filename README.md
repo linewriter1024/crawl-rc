d51d2375c289f1dfac1d15f1637b577c2e5aa8e1
Beha <shacknetisp@mail.com>
2018-11-05 08:26:19 -0500

    Reorganization, allow files on compile.sh args.

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 
 # Compiled
 A compiled version can be found on CBRO: [Nornya.rc](http://crawl.berotato.org/crawl/rcfiles/crawl-git/Nornya.rc)
-
-# Included
-* `Nornya`: My personal config.
-  * `Nornya_Stick`: Options that "stick" beyond each session like macros.
--- compile.sh
+++ compile.sh
@@ -6,9 +6,6 @@ test -e compile.sh || exit 1
 
 ROOT="$(pwd)"
 
-TMP="/tmp/crc_$RANDOM"
-touch "$TMP"
-
 reset_lua() {
     echo ": --"
 }
@@ -23,6 +20,24 @@ cat core/header
 echo
 echo "#-#-# Units Begin"
 
+process_file() {
+    file="$1"
+    dext=${file##*.}
+    ext=${dext#.}
+
+    export CRC_FILE="$file"
+    export CRC_DIR="$(basename "$(dirname "$file")")"
+    export CRC_NAME="$(basename "$file" .$ext)"
+
+    echo
+    echo "#-#-# Unit: $CRC_NAME ($CRC_DIR/$CRC_NAME.$ext)"
+
+    test -e "$ROOT/handlers/$ext" && {
+        reset_lua
+        bash "$ROOT/handlers/$ext"
+    } || echo "No handler for extension: $ext" > /dev/stderr
+}
+
 process_dir() {
     dir="$1"
     for file in "$dir"/*; do
@@ -31,28 +46,19 @@ process_dir() {
         if test -d "$file"; then
             process_dir "$file"
         else
-            dext=${file##*.}
-            ext=${dext#.}
-
-            export CRC_FILE="$file"
-            export CRC_DIR="$(basename "$(dirname "$file")")"
-            export CRC_NAME="$(basename "$file" .$ext)"
-
-            echo
-            echo "#-#-# Unit: $CRC_NAME ($CRC_DIR/$CRC_NAME.$ext)"
-
-            test -e "$ROOT/handlers/$ext" && {
-                reset_lua
-                bash "$ROOT/handlers/$ext"
-            } || echo "No handler for extension: $ext" > /dev/stderr
+            process_file "$file"
         fi
     done
 }
 
-for dir in "$@"; do
-    test -d "$dir" || { echo "Not a directory: $dir" > /dev/stderr; exit 1; }
+for path in "$@"; do
+    test -e "$path" || { echo "Does not exist: $path" > /dev/stderr; exit 1; }
 
-    process_dir "$dir"
+    if test -d "$path"; then
+        process_dir "$path"
+    else
+        process_file "$path"
+    fi
 done
 
 echo
@@ -60,5 +66,3 @@ echo "#-#-# Units End"
 echo "#-#-# Footer"
 reset_lua
 cat core/footer
-
-rm "$TMP"
