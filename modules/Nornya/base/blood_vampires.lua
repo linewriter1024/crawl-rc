945508814c37903215476d540edbc71c7760572b
Beha <shacknetisp@mail.com>
2019-05-07 07:44:42 -0400

    blood_vampires.lua

--- modules/Nornya/base/blood_vampires.lua
+++ modules/Nornya/base/blood_vampires.lua
@@ -1,4 +1,5 @@
 if nrc.features.blood_vampires and you.race() == "Vampire" then
+	crawl.setopt("item_slot += potion of blood:q")
 	crawl.setopt("spell_slots += regeneration:R")
 	crawl.setopt("macros += M 6 qqzR5")
 end
