0db22a63694b129afb84e54182fbf2fab8a38fb8
Beha <shacknetisp@mail.com>
2019-05-11 10:42:39 -0400

    GnWn get skill menu open at start.

--- modules/skill_menu.lua
+++ modules/skill_menu.lua
@@ -1,6 +1,7 @@
 -- Display skill menu on game start.
 nrc.register_ready_start(function()
-	if nrc.features.you_select_skills then
+	-- If you select your skills or have random skills.
+	if nrc.features.you_select_skills or you.class() == "Wanderer" then
 		crawl.sendkeys("m")
 	end
 end)
