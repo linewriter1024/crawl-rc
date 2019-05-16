2b8546c4f7f40c203143a8ada69d3e93739bd10e
Beha <shacknetisp@mail.com>
2019-05-16 11:09:29 -0400

    Better two state vampire detection.

--- core/header/features.lua
+++ core/header/features.lua
@@ -5,7 +5,7 @@ nrc.features = f
 local v = nrc.check_version
 
 -- You are a two-state vampire.
-f.you_two_state_vampire = you.race() == "Vampire" and you.ability_table().f == "Exsanguinate"
+f.you_two_state_vampire = you.race() == "Vampire" and ({alive = true, bloodless = true})[you.hunger_name()]
 -- You are a blood-potion vampire.
 f.you_blood_vampire = you.race() == "Vampire" and not f.you_two_state_vampire
 
