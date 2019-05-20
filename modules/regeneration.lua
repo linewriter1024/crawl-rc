4b96dc8d2ae237c54017a684ee6a22db9d53d4b1
Beha <shacknetisp@mail.com>
2019-05-19 20:50:47 -0400

    Regenerate whenever poisoned.

--- modules/regeneration.lua
+++ modules/regeneration.lua
@@ -7,7 +7,7 @@ nrc.settings.default("regen_mp_limit", 0.5)
 nrc.register_ready(function()
 	local hp, mhp = you.hp()
 	local mp, mmp = you.mp()
-	if not you.regenerating() and nrc.you.castable("Regeneration") and hp < mhp * nrc.settings.get("regen_hp_limit") and mp >= mmp * nrc.settings.get("regen_mp_limit") and you.feel_safe() then
+	if not you.regenerating() and nrc.you.castable("Regeneration") and (hp < mhp * nrc.settings.get("regen_hp_limit") or you.poisoned()) and mp >= mmp * nrc.settings.get("regen_mp_limit") and you.feel_safe() then
 		nrc.you.cast("Regeneration")
 	end
 end)
