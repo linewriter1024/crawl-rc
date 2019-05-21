7715f9f265e4d73aa62695ff03981eb35d9914b6
Beha <shacknetisp@mail.com>
2019-05-21 11:34:51 -0400

    Remove useless line.

--- modules/autocast/deflect_missiles.lua
+++ modules/autocast/deflect_missiles.lua
@@ -1,7 +1,6 @@
 nrc.settings.default("dmsl_mp_limit", 0.75)
 
 nrc.register_ready(function()
-	local hp, mhp = you.hp()
 	local mp, mmp = you.mp()
 	if not you.status("deflect missiles") and nrc.you.castable("Deflect Missiles") and mp >= mmp * nrc.settings.get("dmsl_mp_limit") and you.feel_safe() then
 		nrc.you.cast("Deflect Missiles")
