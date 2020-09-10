13f6e31404f8acef91b33b65e6448742bb271f50
Beha <shacknetisp@mail.com>
2020-09-10 11:21:18 -0400

    Hunger no longer exists.

--- core/header/features.lua
+++ core/header/features.lua
@@ -5,7 +5,7 @@ nrc.features = f
 local v = nrc.check_version
 
 -- You are a two-state vampire.
-f.you_two_state_vampire = you.race() == "Vampire" and ({alive = true, bloodless = true})[you.hunger_name()]
+f.you_two_state_vampire = you.race() == "Vampire" and (not you.hunger_name) or ({alive = true, bloodless = true})[you.hunger_name()]
 -- You are a blood-potion vampire.
 f.you_blood_vampire = you.race() == "Vampire" and not f.you_two_state_vampire
 
