88210a558874ea7b4f5f0d43cba62ea7d517182a
Beha <shacknetisp@mail.com>
2020-11-17 12:22:09 -0500

    Only try to butcher if hunger exists.

--- modules/butcher_override/action.lua
+++ modules/butcher_override/action.lua
@@ -3,7 +3,7 @@ function butcher_override()
 	if nrc.you.castable("Animate Skeleton") and nrc.world.has_skeleton(0, 0) then
 		nrc.you.cast("Animate Skeleton")
 	-- If no special option, just butcher as usual.
-	else
+	elseif nrc.features.hunger_exists then
 		crawl.do_commands{"CMD_BUTCHER"}
 	end
 end
