e128a13afd901c39ab2edf811a3ac6a1293e8207
Beha <shacknetisp@mail.com>
2022-02-20 19:29:32 -0500

    New F-keys

--- modules/Nornya/macros/spell_fn_macros.bash
+++ modules/Nornya/macros/spell_fn_macros.bash
@@ -7,6 +7,7 @@ alphabet="abcdefghijklmnopqrstuvwxyz"
 for i in $(seq 0 10); do
 	# WebTiles
 	echo "macros += M \{-$(expr 1011 + $i)} z${alphabet:$i:1}"
+	echo "macros += M \{-$(expr 265 + $i)} z${alphabet:$i:1}"
 	# Local
 	echo "macros += M \{-$(expr 1073741882 + $i)} z${alphabet:$i:1}"
 done
